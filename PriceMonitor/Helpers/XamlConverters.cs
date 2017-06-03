d2b9336011762fff68edcea5b99159e0753950bf
vargur <none@gmail.com>
2017-06-03 17:07:22 +0300

    deviding

--- PriceMonitor/Helpers/XamlConverters.cs
+++ PriceMonitor/Helpers/XamlConverters.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Globalization;
 using System.Windows.Data;
+using System.Windows.Markup;
 
 namespace PriceMonitor.Helpers
 {
@@ -58,6 +59,30 @@ namespace PriceMonitor.Helpers
 			}
 
 			return null;
+		}
+	}
+
+	public class PercentageConverter : MarkupExtension, IValueConverter
+	{
+		private static PercentageConverter _instance;
+
+		#region IValueConverter Members
+
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return System.Convert.ToDouble(value) * System.Convert.ToDouble(parameter);
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+
+		#endregion
+
+		public override object ProvideValue(IServiceProvider serviceProvider)
+		{
+			return _instance ?? (_instance = new PercentageConverter());
 		}
 	}
 }
